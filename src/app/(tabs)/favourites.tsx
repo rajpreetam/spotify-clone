9b83cab703172e14c0907891a2ecfae041dccaca
Preetam Raj <dev.preetamraj@gmail.com>
2023-09-26 01:06:37 +0530

    initialized project

---
+++ src/app/(tabs)/favourites.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from '../../components/Themed';
+
+const favouritesScreen = () => {
+  return (
+    <View>
+      <Text>favourites</Text>
+    </View>
+  );
+};
+
+export default favouritesScreen;
---
+++ src/app/(tabs)/search.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+import { View } from '../../components/Themed';
+
+export default function SearchScreen() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
