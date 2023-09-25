7833c7d6198728bb372a957b853021df1bf57295
Preetam Raj <dev.preetamraj@gmail.com>
2023-09-26 02:41:38 +0530

    updated color scheme

--- src/app/(tabs)/search.tsx
+++ src/app/(tabs)/search.tsx
@@ -1,12 +1,14 @@
 import { FontAwesome } from '@expo/vector-icons';
 import { useState } from 'react';
-import { FlatList, StyleSheet, TextInput } from 'react-native';
+import { FlatList, StyleSheet, TextInput, useColorScheme } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { tracks } from '../../assets/data/tracks';
 import { Text, View } from '../../components/Themed';
 import TrackListItem from '../../components/TrackListItem';
 
 export default function SearchScreen() {
+  const theme = useColorScheme();
+
   const [search, setSearch] = useState('');
   return (
     <SafeAreaView>
@@ -14,14 +16,15 @@ export default function SearchScreen() {
         <FontAwesome name="search" size={16} color="gray" />
         <TextInput
           placeholder="What do you want to listen to..."
-          style={styles.input}
+          style={[
+            styles.input,
+            { backgroundColor: theme === 'light' ? 'lightgray' : '#121314' },
+          ]}
           placeholderTextColor="gray"
           value={search}
           onChangeText={setSearch}
         />
-        <Text onPress={() => setSearch('')} style={{ color: 'white' }}>
-          Cancel
-        </Text>
+        <Text onPress={() => setSearch('')}>Cancel</Text>
       </View>
       <FlatList
         data={tracks}
