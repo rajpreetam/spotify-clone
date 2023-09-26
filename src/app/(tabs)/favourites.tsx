57fb545dcbd773d5ed42a2ae65873d3950b42959
Preetam Raj <dev.preetamraj@gmail.com>
2023-09-26 18:11:40 +0530

    completed the whole app

--- src/app/(tabs)/favourites.tsx
+++ src/app/(tabs)/favourites.tsx
@@ -1,8 +1,44 @@
-import { FlatList } from 'react-native';
-import { tracks } from '../../assets/data/tracks';
+import { gql, useQuery } from '@apollo/client';
+import { ActivityIndicator, FlatList } from 'react-native';
 import TrackListItem from '../../components/TrackListItem';
 
+const query = gql`
+  query MyQuery($userId: String!) {
+    favoritesByUser_id(user_id: $userId) {
+      id
+      track_id
+      user_id
+      track {
+        id
+        name
+        preview_url
+        artists {
+          id
+          name
+        }
+        album {
+          id
+          name
+          images {
+            height
+            url
+            width
+          }
+        }
+      }
+    }
+  }
+`;
+
 const FavouritesScreen = () => {
+  const { data, loading, error } = useQuery(query, {
+    variables: { userId: 'Preetam' },
+  });
+
+  if (loading) return <ActivityIndicator />;
+
+  const tracks = (data?.favoritesByUser_id ?? []).map((fav: any) => fav.track);
+
   return (
     <FlatList
       data={tracks}
--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,16 +1,54 @@
+import { gql, useMutation, useQuery } from '@apollo/client';
 import { Ionicons } from '@expo/vector-icons';
 import { AVPlaybackStatus, Audio } from 'expo-av';
 import { Sound } from 'expo-av/build/Audio';
 import React, { useEffect, useState } from 'react';
-import { Image, StyleSheet } from 'react-native';
+import { Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { useSelector } from 'react-redux';
 import { RootState } from '../features/store';
 import { Text, View } from './Themed';
+
+const insertFavouriteMutation = gql`
+  mutation MyMutation($trackId: String!, $userId: String!) {
+    insertFavorites(track_id: $trackId, user_id: $userId) {
+      id
+      track_id
+      user_id
+    }
+  }
+`;
+
+const isFavouriteQuery = gql`
+  query MyQuery($trackId: String!, $userId: String!) {
+    favoritesByTrack_idAndUser_id(track_id: $trackId, user_id: $userId) {
+      id
+      track_id
+      user_id
+    }
+  }
+`;
+
+const deleteFavouriteMutation = gql`
+  mutation MyMutation($trackId: String!, $userId: String!) {
+    deleteFavorites(track_id: $trackId, user_id: $userId) {
+      id
+    }
+  }
+`;
 
 const Player = () => {
   const track = useSelector((state: RootState) => state.player.track);
   const [sound, setSound] = useState<Sound>();
   const [isPlaying, setIsPlaying] = useState(false);
+
+  const [insertFavourite] = useMutation(insertFavouriteMutation);
+  const [deleteFavourite] = useMutation(deleteFavouriteMutation);
+
+  const { data, refetch } = useQuery(isFavouriteQuery, {
+    variables: { userId: 'Preetam', trackId: track?.id ?? '' },
+  });
+
+  const isLiked = data?.favoritesByTrack_idAndUser_id?.length > 0;
 
   const onPlaybackSTatusUpdate = (status: AVPlaybackStatus) => {
     if (!status.isLoaded) return;
@@ -35,6 +73,21 @@ const Player = () => {
 
     if (isPlaying) await sound.pauseAsync();
     else await sound.playAsync();
+  };
+
+  const onLike = async () => {
+    if (!track) return;
+
+    if (isLiked) {
+      await deleteFavourite({
+        variables: { userId: 'Preetam', trackId: track?.id },
+      });
+    } else {
+      await insertFavourite({
+        variables: { userId: 'Preetam', trackId: track?.id },
+      });
+    }
+    refetch();
   };
 
   useEffect(() => {
@@ -63,20 +116,23 @@ const Player = () => {
           <Text style={styles.subTitle}>{track.artists[0]?.name}</Text>
         </View>
 
-        <Ionicons
-          name="heart-outline"
-          size={20}
-          color="white"
-          style={{ marginHorizontal: 8 }}
-        />
+        <TouchableOpacity onPress={onLike}>
+          <Ionicons
+            name={isLiked ? 'heart' : 'heart-outline'}
+            size={20}
+            color="white"
+            style={{ marginHorizontal: 8 }}
+          />
+        </TouchableOpacity>
 
-        <Ionicons
-          onPress={onPlayPause}
-          disabled={!track?.preview_url}
-          name={isPlaying ? 'pause' : 'play'}
-          size={24}
-          color={track?.preview_url ? 'white' : 'gray'}
-        />
+        <TouchableOpacity onPress={onPlayPause}>
+          <Ionicons
+            disabled={!track?.preview_url}
+            name={isPlaying ? 'pause' : 'play'}
+            size={24}
+            color={track?.preview_url ? 'white' : 'gray'}
+          />
+        </TouchableOpacity>
       </View>
     </View>
   );
