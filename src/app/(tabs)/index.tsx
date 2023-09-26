722cb66accdac466dd3135af05659947ff18dece
Preetam Raj <dev.preetamraj@gmail.com>
2023-09-26 16:06:34 +0530

    implemented home screen for recommendations

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,8 +1,43 @@
-import { FlatList } from 'react-native';
-import { tracks } from '../../assets/data/tracks';
+import { gql, useQuery } from '@apollo/client';
+import { ActivityIndicator, FlatList } from 'react-native';
+import { Text } from '../../components/Themed';
 import TrackListItem from '../../components/TrackListItem';
 
+const query = gql`
+  query MyQuery($genres: String!) {
+    recommendations(seed_genres: $genres) {
+      tracks {
+        id
+        name
+        preview_url
+        artists {
+          id
+          name
+        }
+        album {
+          id
+          name
+          images {
+            height
+            url
+            width
+          }
+        }
+      }
+    }
+  }
+`;
+
 const HomeScreen = () => {
+  const { data, loading, error } = useQuery(query, {
+    variables: { genres: 'indian, indie-pop' },
+  });
+
+  if (loading) return <ActivityIndicator />;
+  if (error) return <Text>Failed to load recommended tracks</Text>;
+
+  const tracks = data?.recommendations?.tracks || [];
+
   return (
     <FlatList
       data={tracks}
